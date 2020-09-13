e1a0c6b06db5441263c81972e979e904147cff22
Naveen Rawat <naveen.rawat@sprint.com>
2020-09-13 15:31:27 +0530

    comps added basic setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', component:  LoginComponent},
+  { path:  'profile-list', component:  ProfileListComponent},
+  { path:  'profile-details/:id', component:  ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileListComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
